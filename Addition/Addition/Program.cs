ac5c4873ef295d09150bbc6d3c5011c0be4f9616
Deep Parmar <deep.parmar@rxteam376.onmicrosoft.com>
2021-08-04 17:55:02 +0530

    add Project

---
+++ Addition/Addition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=4,b=5;
+            Console.WriteLine(a+b);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
